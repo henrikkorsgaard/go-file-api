cb50124dabdeba49c10071f3aea433749fbe492a
Henrik Korsgaard <40322+henrikkorsgaard@users.noreply.github.com>
2023-03-19 19:01:11 +0100

    created methods for posting and getting json to/from files. Test passes!

--- api/api.go
+++ api/api.go
@@ -2,27 +2,120 @@ package api
 
 import (
 	"fmt"
+	"strings"
+	"io"
+	"os"
 	"log"
 	"net/http"
+	"net/url"
+	"encoding/json"
+	"errors"
 )
 
-func StartAPIServerOnPort(port int){
-	addr := fmt.Sprintf(":%d", port)
-	http.HandleFunc("/", apiRouter)
-	log.Fatal(http.ListenAndServe(addr, nil))
+
+type API struct {
+	//need a file folder config to store files in specific folder
 }
 
-// https://restfulapi.net/http-status-codes/
-func apiRouter(w http.ResponseWriter, r *http.Request){
-	
+
+func (api API) ServeHTTP(w http.ResponseWriter, r *http.Request){
+
 	if r.Method == "GET" {
-		fmt.Fprintf(w,"")
+		entity := getEntityFromPath(r.URL)
+		json, err := readFromFile(entity)
+		if err != nil {
+			w.WriteHeader(503)
+			fmt.Fprintf(w,"503: Service unavailable")
+			return
+		}
+
+		json = fmt.Sprintf("[%s]",json)
+		fmt.Fprintf(w,json)
 	} else if r.Method == "POST" {
-		fmt.Fprintf(w,"")
+		
+		//we try to get the first entity. That's all we are interested in at this point
+		entity := getEntityFromPath(r.URL)
+		
+		// Return 421: Not Found because the API does not accept post on /
+		if entity == "" {
+			w.WriteHeader(421)
+			fmt.Fprintf(w,"421: Cannot accept POST to /")
+			return
+		}
+		
+		//Maybe just try to demarshal and catch errors
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var jsonObj interface{}
+		err = json.Unmarshal(body, &jsonObj)
+		if err != nil {
+			w.WriteHeader(507)
+			fmt.Fprintf(w,"507: Unable to store entity due to unknown error")
+			return
+		}
+
+		//At this point we trust that the incoming string is parsable by the json library, hence we can store it!
+		err = writeToFile(entity, string(body))
+		
+		if err != nil {
+			w.WriteHeader(507)
+			fmt.Fprintf(w,"507: Unable to store entity due to unknown error")
+			return
+		} else {
+			w.WriteHeader(201)
+			fmt.Fprintf(w,"201: Entity created")
+			return
+		}
+
 	} else {
 		w.WriteHeader(400)
+		fmt.Fprintf(w,"400")
+	}
+}
+
+func getEntityFromPath(url *url.URL) (entity string) {
+
+	splitFunc := func(c rune) bool {
+		return c == '/'
 	}
 
-	//GET
-	//POST -> if t
+	elements := strings.FieldsFunc(url.Path, splitFunc)
+
+	if len(elements) > 0 {
+		entity = elements[0]
+	}
+	return 
 }
+
+func writeToFile(entity string, json string) error {
+	filename := fmt.Sprintf("%s.henrik", entity)
+
+	fileNotExisting := false;
+
+	if _, unerr := os.Stat(filename); errors.Is(unerr, os.ErrNotExist) {
+		fileNotExisting = true
+	}
+
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	} 
+	defer file.Close()
+
+	line := fmt.Sprintf(",\n%s", json) //because we need to add , to fix the array
+	if fileNotExisting {
+		line = fmt.Sprintf("%s", json) //because we need to add , to fix the array
+	}
+	_, err = file.WriteString(line)
+	return err
+}
+
+func readFromFile(entity string) (string, error){
+	filename := fmt.Sprintf("%s.henrik", entity)
+	data, err := os.ReadFile(filename)
+ 
+	return string(data), err
+} 
--- integration_test.go
+++ integration_test.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"bytes"
 	"testing"
 	"net/http"
+	"io"
+	"encoding/json"
 	"github.com/henrikkorsgaard/go-file-api/api"
 )
 
@@ -14,45 +17,99 @@ import (
 	When possible, I will refactor into isolated test cases
 */
 
-
-func TestAPIServerCRUD(t *testing.T){
-	port := 3000
-	go api.StartAPIServerOnPort(port) // how the fuck do I stop this again
-
-	url := fmt.Sprintf("http://localhost:%d/", port)
-
-	t.Run("GET", func(t *testing.T){
-		res, err := http.Get(url)
-		defer res.Body.Close()
-		if err != nil {
-			t.Fatalf("Server responded with error: %v ", err)
-		}
-	
-		if res.StatusCode != 200 {
-			t.Fatalf("Server responded with status code: %v", res.StatusCode)
-		}
-	})
-
-	t.Run("POST" , func(t *testing.T){
-		res, err := http.Post(url, "application/json",nil)
-
-		if err != nil {
-			t.Fatalf("Server responded with error: %v ", err)
-		}
-	
-		if res.StatusCode != 200 {
-			t.Fatalf("Server responded with status code: %v", res.StatusCode)
-		}
-	})
-	
-	// TEAR DOWN SHOULD BE STOPPING SERVER!
+type Todo struct {
+	Todo string `json:"todo"`
+	Due string `json:"due"`
 }
 
 
-//First test starting a webserver on port?
+func TestAPIServer(t *testing.T){
+	port := 3000
+	addr := fmt.Sprintf(":%d", port)
 
-//second test post JSON
+	go  http.ListenAndServe(addr, api.API{})// how the fuck do I stop this again
 
-//third test get JSON that matches the posted json
+	url := fmt.Sprintf("http://localhost:%d/test-todo", port)
 
-//authentication
+	t.Run("POST /", func(t *testing.T){
+		rootUrl := fmt.Sprintf("http://localhost:%d", port)
+		req, err := http.NewRequest("POST", rootUrl, bytes.NewBuffer([]byte("")))
+
+		// This should not happen 
+		if err != nil {
+			t.Fatal("Programmer error!")
+		}
+
+		req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+
+		client := &http.Client{}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Logf("Server responded with an error: %v", err)
+			t.Fail()
+		}
+		defer resp.Body.Close()
+	
+		// We need to make sure that the resource is created!
+		if resp.StatusCode != 421 {
+			t.Logf("Server responded with status code: %v, expected 421", resp.StatusCode)
+			t.Fail()
+		}
+	})
+
+	t.Run("POST json" , func(t *testing.T){
+
+		marshal_todo, err := json.Marshal(Todo{Todo:"Clean kitchen", Due:"today"})
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(marshal_todo))
+		
+		// This should not happen 
+		if err != nil {
+			t.Fatal("Programmer error!")
+		}
+
+		req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+
+		client := &http.Client{}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Logf("Server responded with an error: %v", err)
+			t.Fail()
+		}
+		defer resp.Body.Close()
+	
+		// We need to make sure that the resource is created!
+		if resp.StatusCode != 201 {
+			t.Logf("Server responded with status code: %v, expected 201", resp.StatusCode)
+			t.Fail()
+		}
+	})
+
+	t.Run("GET json", func(t *testing.T){
+		resp, err := http.Get(url)
+		defer resp.Body.Close()
+		if err != nil {
+			t.Logf("Server responded with error: %v ", err)
+			t.Fail()
+		}
+	
+		if resp.StatusCode != 200 {
+			t.Logf("Server responded with status code: %v", resp.StatusCode)
+			t.Fail()
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Logf("Unable to read response body from server: %v ", err)
+			t.Fail()
+		}
+
+		var todos []Todo
+		err = json.Unmarshal(body, &todos)
+		if err != nil {
+			t.Logf("Unable to unmarshal todos from server: %v ", err)
+			t.Fail()
+		}
+	})
+}
